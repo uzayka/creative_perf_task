5dd8e42fef0fed30f82f2fa2a326c71548cce055
uzayka <uzaynetdinova@gmail.com>
2026-03-20 13:07:16 +0400

    Update parolchiki.py

--- parolchiki.py
+++ parolchiki.py
@@ -26,4 +26,19 @@ def generate_password(lowercase, numbers, uppercase, signs, num_1, num_2, num_3,
     return ''.join(password)
 
 
+def check_safety(password):
+    length = len(password)
+    has_lower = any(c.islower() for c in password)
+    has_upper = any(c.isupper() for c in password)
+    has_digit = any(c.isdigit() for c in password)
+    has_spec = any(c in '!@#$%^&*()' for c in password)
+
+    types_count = sum([has_lower, has_upper, has_digit, has_spec])
+
+    if length >= 12 and types_count >= 3:
+        return "This password is safe ^^", "green"
+    elif length >= 8 and types_count >= 2:
+        return "This is not the best option. Try to generate again", "orange"
+    else:
+        return "It is not a safe option:(", "red"
 
